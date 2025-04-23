837b24eaca2189ba2915f7542ebbb7be559d2576
hyunw00 <khwland090@gmail.com>
2025-04-24 02:14:38 +0900

    🎉  init: 프로젝트 생성

---
+++ src/app/(with-searchbar)/page.tsx
@@ -0,0 +1,53 @@
+import BookItem from "@/components/book-item";
+import style from "./page.module.css";
+import { BookData } from "@/types";
+
+async function AllBooks() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
+    { cache: "no-store" }
+  );
+
+  if (!response.ok) {
+    return <div>오류 발생 🚨</div>;
+  }
+  const allBooks: BookData[] = await response.json();
+
+  return (
+    <section>
+      <h3>등록된 모든 도서</h3>
+      {allBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </section>
+  );
+}
+
+async function RandomBooks() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
+    { next: { revalidate: 3 } }
+  );
+  if (!response.ok) {
+    return <div>오류 발생 🚨</div>;
+  }
+  const randomBooks: BookData[] = await response.json();
+
+  return (
+    <section>
+      <h3>지금 추천하는 도서</h3>
+      {randomBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </section>
+  );
+}
+
+export default function Home() {
+  return (
+    <div className={style.container}>
+      <RandomBooks />
+      <AllBooks />
+    </div>
+  );
+}
---
+++ src/app/(with-searchbar)/search/page.tsx
@@ -0,0 +1,24 @@
+import BookItem from "@/components/book-item";
+import { BookData } from "@/types";
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ q?: string }>;
+}) {
+  const { q } = await searchParams;
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`
+  );
+  if (!response.ok) return <div>오류 발생</div>;
+  const searchBooks: BookData[] = await response.json();
+
+  return (
+    <div>
+      {searchBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
