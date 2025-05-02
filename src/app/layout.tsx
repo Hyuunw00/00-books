53ea1231800ccd590ab2117af3c94c4ad09e338c
hyunw00 <khwland090@gmail.com>
2025-05-03 01:29:13 +0900

    ✨ feat: 라우트 인터셉트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import Link from "next/link";
 import style from "./layout.module.css";
+import { ReactNode } from "react";
 
 async function Footer() {
   const response = await fetch(
@@ -23,8 +24,10 @@ async function Footer() {
 
 export default function RootLayout({
   children,
+  modal,
 }: Readonly<{
   children: React.ReactNode;
+  modal: ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -34,8 +37,10 @@ export default function RootLayout({
             <Link href={"/"}>📚 ONEBITE BOOKS</Link>
           </header>
           <main>{children}</main>
-          {/* <Footer /> */}
+          <Footer />
         </div>
+        {modal}
+        <div id="root-modal" />
       </body>
     </html>
   );
---
+++ src/components/modal.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { ReactNode, useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+import style from "./modal.module.css";
+import { useRouter } from "next/navigation";
+
+export default function Modal({ children }: { children: ReactNode }) {
+  const dialogRef = useRef<HTMLDialogElement>(null);
+  const router = useRouter();
+  useEffect(() => {
+    if (!dialogRef.current?.open) {
+      dialogRef.current?.showModal();
+
+      dialogRef.current?.scrollTo({
+        top: 0,
+      });
+    }
+  }, []);
+
+  return createPortal(
+    <dialog
+      onClose={(e) => router.back()} // esc 눌러서 모달창  꺼지면  이전 경로로 이동
+      onClick={(e) => {
+        if ((e.target as HTMLElement).nodeName === "DIALOG") router.back(); // 모달 바깥 누르면 이전 경로로 이동
+      }}
+      className={style.modal}
+      ref={dialogRef}
+    >
+      {children}
+    </dialog>, // 기본적으로 비활성화 상태가됨
+    document.getElementById("root-modal") as HTMLEmbedElement
+  );
+}
